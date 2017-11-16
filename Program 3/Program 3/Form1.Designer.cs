6458cdd585f1a9d4bf8b1aa684f3c75aa39b49a7
Tony Piccirilli <anpicc01@louisville.edu>
2017-11-16 08:38:13 -0500

    Initial commit

---
+++ Program 3/Program 3/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Program_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.creditBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.creditHoursLabel = new System.Windows.Forms.Label();
+            this.timeAnswer = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(31, 53);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 0;
+            // 
+            // creditBox
+            // 
+            this.creditBox.Location = new System.Drawing.Point(235, 53);
+            this.creditBox.Name = "creditBox";
+            this.creditBox.Size = new System.Drawing.Size(100, 20);
+            this.creditBox.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(25, 37);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(106, 13);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Write your last name:";
+            // 
+            // creditHoursLabel
+            // 
+            this.creditHoursLabel.AutoSize = true;
+            this.creditHoursLabel.Location = new System.Drawing.Point(188, 37);
+            this.creditHoursLabel.Name = "creditHoursLabel";
+            this.creditHoursLabel.Size = new System.Drawing.Size(183, 13);
+            this.creditHoursLabel.TabIndex = 3;
+            this.creditHoursLabel.Text = "How many credit hours do you have?";
+            // 
+            // timeAnswer
+            // 
+            this.timeAnswer.Location = new System.Drawing.Point(94, 143);
+            this.timeAnswer.Name = "timeAnswer";
+            this.timeAnswer.Size = new System.Drawing.Size(194, 23);
+            this.timeAnswer.TabIndex = 6;
+            this.timeAnswer.Text = "\r\n";
+            this.timeAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(121, 121);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(129, 13);
+            this.timeLabel.TabIndex = 7;
+            this.timeLabel.Text = "Earliest Registration Time:";
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(56, 214);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 8;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(161, 214);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(260, 214);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 273);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.timeAnswer);
+            this.Controls.Add(this.creditHoursLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.creditBox);
+            this.Controls.Add(this.nameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox creditBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label creditHoursLabel;
+        private System.Windows.Forms.Label timeAnswer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Program 3/Program 3/Form1.cs
@@ -0,0 +1,152 @@
+﻿// Tony Piccirilli
+// CIS 199-01-4162
+// Program 3
+// April 5, 2016
+// This program takes the student's last name
+// and how many credit hours the student has
+// to determine their earliest registration time and day.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+                const float SENIOR_HOURS = 90;    // Min hours for Senior
+                const float JUNIOR_HOURS = 60;    // Min hours for Junior
+                const float SOPHOMORE_HOURS = 30; // Min hours for Soph.
+
+                const string DAY1 = "March 30";  // 1st day of registration
+                const string DAY2 = "March 31";  // 2nd day of registration
+                const string DAY3 = "April 1";   // 3rd day of registration
+                const string DAY4 = "April 4";   // 4th day of registration
+                const string DAY5 = "April 5";   // 5th day of registration
+                const string DAY6 = "April 6";   // 6th day of registration
+
+                const string TIME1 = "8:30 AM";  // 1st time block
+                const string TIME2 = "10:00 AM"; // 2nd time block
+                const string TIME3 = "11:30 AM"; // 3rd time block
+                const string TIME4 = "2:00 PM";  // 4th time block
+                const string TIME5 = "4:00 PM";  // 5th time block
+
+                string lastNameStr;       // Entered last name
+                char lastNameLetterCh;    // First letter of last name, as char
+                string dateStr = "Error"; // Holds date of registration
+                string timeStr = "Error"; // Holds time of registration
+                float creditHours;        // Entered credit hours
+                
+
+                if (float.TryParse(creditBox.Text, out creditHours) && creditHours >= 0) // Valid hours
+                {
+                    lastNameStr = nameBox.Text;
+                    if (lastNameStr.Length > 0) // Empty string?
+                    {
+                        lastNameStr = lastNameStr.ToUpper(); // Ensure upper case
+                        lastNameLetterCh = lastNameStr[0];   // First char of last name
+
+                        if (char.IsLetter(lastNameLetterCh)) // Is it a letter?
+                        {
+                            // Juniors and Seniors share same schedule but different days
+                            if (creditHours >= JUNIOR_HOURS)
+                            {
+                                if (creditHours >= SENIOR_HOURS)
+                                    dateStr = DAY1;
+                                else // Must be juniors
+                                    dateStr = DAY2;
+
+                                
+                                char[] lastName = { 'A', 'E', 'J', 'P', 'T', };  // Last Name Array
+                                string[] times = { TIME5, TIME1, TIME2, TIME3, TIME4 };  // Time frame for each last name.
+                                int index;                                              // Array Subsricpt
+                                 
+                                 index = lastName.Length - 1;
+                                 while (index >= 0 && lastNameLetterCh < lastName[index])
+                                    --index;
+
+                                    timeStr = times[index];
+                                   
+                                
+                        }
+                            else // Must be soph/fresh
+                            {
+                                if (creditHours >= SOPHOMORE_HOURS)
+                                {
+                                    // E-Q on one day
+                                    if ((lastNameLetterCh >= 'E') && // >= E and
+                                        (lastNameLetterCh <= 'Q'))   // <= Q
+                                        dateStr = DAY3;
+                                    else // All other letters on next day
+                                        dateStr = DAY4;
+                                }
+                                else // must be freshman
+                                {
+                                    // E-Q on one day
+                                    if ((lastNameLetterCh >= 'E') && // >= E and
+                                        (lastNameLetterCh <= 'Q'))   // <= Q
+                                        dateStr = DAY5;
+                                    else // All other letters on next day
+                                        dateStr = DAY6;
+                                }
+                               
+                                
+                                char[] lastName = { 'A', 'C', 'E', 'G', 'J', 'M', 'P', 'R', 'T', 'W' };   // Array to find last name.
+                                string[] times = { TIME4, TIME5, TIME1, TIME2, TIME3, TIME4, TIME5, TIME1, TIME2, TIME3 };  //Array to find time frame.
+                                int index;   // Array Subsricpt
+                                
+                                index = lastName.Length - 1;
+                                while (index >= 0 && lastNameLetterCh < lastName[index])
+                                    --index;
+                                    
+                                    timeStr = times[index];
+                                    
+                                
+                            }
+                            // Output results
+                            timeAnswer.Text = dateStr + " at " + timeStr;
+                        
+                        }
+                        else // First char not a letter
+                            MessageBox.Show("Enter valid last name!");
+                    }
+                      else // Empty textbox
+                    MessageBox.Show("Enter a last name!");
+                }
+                else // Can't parse credit hours
+                    MessageBox.Show("Please enter valid credit hours earned!");
+                }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+           // To clear all textboxes and labels.
+            creditBox.Text = "";
+            nameBox.Text = "";
+            timeAnswer.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // To close the form.
+            this.Close();
+        }
+
+
+
+
+        }
+        
+        
+    }
